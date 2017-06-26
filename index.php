a9e0be32c9a538ba34c4cca01606f21ca72cf0ec
stevenkellow <steven.kellow@gmail.com>
2017-06-26 18:34:19 +0100

    Bump version number

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: WPJM Schema
 Plugin URI: https://wordpress.org/plugins/wpjm-schema/
 Description: Adds Schema.org markup to your WP Job Manager pages
-Version: 0.1
+Version: 0.2
 Author: Steven Kellow
 Author URI: https://www.stevenkellow.com
 Text Domain: wpjm-schema
